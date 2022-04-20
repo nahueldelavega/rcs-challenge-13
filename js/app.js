204335c0332468aebe6db64a2be51e29f5f7856c
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-04-20 15:18:14 -0300

    challenge 13 listo

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let firstNumber = prompt('Decime un numerito');
+let secondtNumber = prompt('Decime otro numerito');
+if(firstNumber<secondtNumber){
+    console.log('Es menor cumpa');
+}
+else if(firstNumber==secondtNumber){
+    console.log('iguales pa');
+}
+else if(firstNumber>secondtNumber){
+    console.log('esta vez es mayor como eia');
+};
